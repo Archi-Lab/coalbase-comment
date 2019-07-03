2efc945fcdf55069c6d855a41481ba298439683a
JannIntveen <JannIntveen@gmail.com>
2019-07-03 15:08:39 +0200

    Added Embedded Kafka

--- src/test/java/de/archilab/coalbase/commentservice/CoalbaseCommentApplicationTest.java
+++ src/test/java/de/archilab/coalbase/commentservice/CoalbaseCommentApplicationTest.java
@@ -2,13 +2,18 @@ package de.archilab.coalbase.commentservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@EmbeddedKafka
 public class CoalbaseCommentApplicationTest {
+
+  @Autowired
+  private EmbeddedKafkaBroker embeddedKafka;
 
   @Test
   public void contextLoads() {
